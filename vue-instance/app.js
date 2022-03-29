e6ad31e65bc5dbf31d79c873240999680bb36ae0
hilalozturk1 <hlloztrk68@gmail.com>
2022-03-29 22:20:01 +0300

    event usage

--- vue-instance/app.js
+++ vue-instance/app.js
@@ -9,12 +9,23 @@ const app = Vue.createApp({
                 url : "https://www.google.com",
                 alt : "google"
             },
-            examp : "attribute"
+            examp : "attribute",
+            coords : {
+                x: 0,
+                y: 0
+            }
         }
     },
     methods: {
         changeTitle(passTitle) {
             this.title = passTitle;
+        },
+        updateCoords(message, event) {
+            // console.log(message,event.x,event.y);
+            this.coords = {
+                x : event.x,
+                y : event.y
+            }
         }
     },
 }).mount("#app");
