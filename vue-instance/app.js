b31c64e7aba509dc6d0453af37fb687ad4e62d64
hilalozturk1 <hlloztrk68@gmail.com>
2022-03-29 20:57:02 +0300

    string interpolation

---
+++ vue-instance/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title : "Title",
+            content : "Content"
+        }
+    },
+}).mount("#app");
+
+
+
+// Vue2 Version
+// new Vue({
+//     data : {
+
+//     }
+// })
