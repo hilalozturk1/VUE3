3517612a3dbc0e26ff941770d5ff67b643f898b1
hilalozturk1 <hlloztrk68@gmail.com>
2022-03-29 23:57:47 +0300

    reactivity-computed

--- counter-app/app.js
+++ counter-app/app.js
@@ -1,7 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0
+            counter: 0,
+            counter2: 0
+        }
+    },
+    methods: {
+    },
+    computed : {
+        //to prevent the entire vue instance from being rendered again, we define it as computed
+        getCounterResult(){
+            console.log('counter worked');
+            return this.counter > 5 ? 'Greater' : 'Less';
+        },
+        getCounter2Result(){
+            console.log('counter2 worked');
+            return this.counter2 > 5 ? 'Greater' : 'Less';
         }
     }
 }).mount("#app")
