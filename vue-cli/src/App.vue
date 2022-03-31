a8bf8c2bc8e63bfae3742ec5a126e7c4550c9ac7
hilalozturk1 <hlloztrk68@gmail.com>
2022-03-31 14:25:49 +0300

    single file component/vue cli/option API

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h3>Vue CLI</h3>
+    <p>Single File Component</p>
+  </div>
+  <!-- <CounterItem></CounterItem> -->
+  <!-- <counter-item></counter-item> -->
+  <counter-item />
+  <counterItem />
+</template>
+<script>
+import CounterItem from "@/components/CounterItem"
+export default {
+  components: {
+    CounterItem,
+    //"counter-item" : CounterItem
+  }
+}
+</script>
---
+++ vue-cli/src/components/CounterItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container-sm">
+        <h3> {{counter}} </h3>
+        <button @click="counter++" class="red">+</button>
+        <button @click="counter--" class="green">-</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            counter : 0
+        }
+    },
+}
+</script>
