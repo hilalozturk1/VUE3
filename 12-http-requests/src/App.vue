653f86a4a3d1934de101d6dc32c64d6b7747c280
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-01 16:33:56 +0300

    using delete methods

--- 12-http-requests/src/App.vue
+++ 12-http-requests/src/App.vue
@@ -8,7 +8,7 @@
     <ul>
       <li v-for="item in itemList" :key="item.id" class="d-flex align-items-center justify-content-between">
         <span> {{item.title}} </span>
-        <button class="red">Delete</button>
+        <button class="red" @click="onDelete(item)">Delete</button>
       </li>
     </ul>
     <span>There are {{itemList.length}} products</span>
@@ -36,14 +36,21 @@ export default {
         created_at : new Date(),
         completed : false
       };
-
+      
       axios.post("http://localhost:3000/items", saveObject).then(save_response => {
         //saving to db.json file
         console.log('save_response', save_response);
+        this.itemList.push(save_response.data);
         event.target.value = "";
         event.target.focus();
       });
-    }
+    },
+    onDelete(item) {
+      axios.delete("http://localhost:3000/items/"+item.id).then(delete_response => {
+        console.log('delete_response', delete_response);
+        this.itemList = this.itemList.filter(i => i.id !== item.id);
+      })
+    },
   },
 }
 </script>
