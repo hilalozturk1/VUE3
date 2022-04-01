1c45028cf734b900cbebdb26c12aa7ab32a7f6e3
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-01 15:24:09 +0300

    todo

---
+++ 12-http-requests/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h3>Shopping list</h3>
+    <hr>
+    <div>
+      <input type="text" placeholder="What will you have?">
+    </div>
+    <ul>
+      <li class="d-flex align-items-center justify-content-between">
+        <span>Item 1</span>
+        <button class="red">Delete</button>
+      </li>
+      <li class="d-flex align-items-center justify-content-between">
+        <span>Item 1</span>
+        <button class="red">Delete</button>
+      </li>
+      <li class="d-flex align-items-center justify-content-between">
+        <span>Item 1</span>
+        <button class="red">Delete</button>
+      </li>
+    </ul>
+    <span>There are 5 products</span>
+  </div>  
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
