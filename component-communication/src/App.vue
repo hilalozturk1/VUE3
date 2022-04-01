4accc6134ccd4d40acbbe36e2e6dd0ea2915b44b
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-01 12:05:03 +0300

    props-emits

---
+++ component-communication/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <UserSection :userList="userList" @new-item="userList.push($event)"/>
+  </div>
+</template>
+<script>
+import UserSection from "@/components/UserSection"
+export default {
+  components: {
+    UserSection
+  },
+  data() {
+    return {
+      userList : ["Name1" , "Name2" , "Name3"]
+    }
+  }
+}
+</script>
---
+++ component-communication/src/components/UserSection.vue
@@ -0,0 +1,14 @@
+<template>
+    <ListSection :userList="userList"/>
+    <button @click="$emit('new-item', new Date().getTime())">Set Data</button>
+</template>
+<script>
+import ListSection from "@/components/ListSection"
+export default {
+    props: ["userList"],
+    emits: ["new-item"], //only vue3 - optional
+    components: { 
+        ListSection 
+    }
+}
+</script>
