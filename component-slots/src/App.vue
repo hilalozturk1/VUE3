9b5e76a38505370b91d9b034a05018695cf9cc7b
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-01 13:42:31 +0300

    title information that comes with the slot-directly from between the component tags

---
+++ component-slots/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Modal :onSave="onSave">
+    <h3>The header information that comes with the slot</h3>
+  </Modal>
+</template>
+<script>
+import Modal from "@/components/ModalSection"
+export default {
+  components:{
+    Modal
+  },
+  methods: {
+    onSave() {
+      alert("Saved!");
+    }
+  },
+}
+</script>
---
+++ component-slots/src/components/ModalSection.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <slot />
+        </div>
+        <hr>
+        <div class="content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quam consequatur id qui dolores quod quasi enim assumenda
+            laborum soluta doloremque molestiae rem, amet dolor natus 
+            doloribus maxime voluptates autem dolorem?
+        </div>
+        <hr>
+        <div class="footer">
+            <button>Close</button>
+            <button class="green" @click="onSave">Submit</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+   props:["onSave"]
+}
+</script>
