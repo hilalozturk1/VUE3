5f0368c8bb09cc7833bf66de7b864203b9b0da3b
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-01 15:59:47 +0300

    axios import / using get&post methods

--- 12-http-requests/src/App.vue
+++ 12-http-requests/src/App.vue
@@ -3,7 +3,7 @@
     <h3>Shopping list</h3>
     <hr>
     <div>
-      <input type="text" placeholder="What will you have?">
+      <input type="text" placeholder="What will you have?" @keydown.enter="onSave">
     </div>
     <ul>
       <li class="d-flex align-items-center justify-content-between">
@@ -23,10 +23,34 @@
   </div>  
 </template>
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
+      itemList : []
+    }
+  },
+  mounted() {
+    axios.get("http://localhost:3000/items").then(items_response => {
+      //fetching from db.json
+      console.log('items_response', items_response);
+      this.itemList = items_response.data || [];
+    })
+  },
+  methods: {
+    onSave(event) {
+      const saveObject = {//create an object
+        title : event.target.value,
+        created_at : new Date(),
+        completed : false
+      };
       
+      axios.post("http://localhost:3000/items", saveObject).then(save_response => {
+        //saving to db.json file
+        console.log('save_response', save_response);
+        event.target.value = "";
+        event.target.focus();
+      });
     }
   },
 }
