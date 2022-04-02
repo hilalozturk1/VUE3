8e3274200849b5900abe3b54eecd45ea3a960b1f
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-02 23:10:18 +0300

    using axios globally

--- 16-vue-router-app/src/main.js
+++ 16-vue-router-app/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import axios from "axios";//like plugin
-
+import {appAxios} from './utils/appAxios';
 const app = createApp(App);
 app.use(router);
 app.config.globalProperties.$axios = axios;//global
+app.config.globalProperties.$appAxios = appAxios;//global
 app.mount('#app');
--- 16-vue-router-app/src/views/NewBookmark.vue
+++ 16-vue-router-app/src/views/NewBookmark.vue
@@ -30,7 +30,7 @@ export default {
     methods: {
         onSave(){
             console.log('this.userData', this.userData);
-            this.$axios.post("http://localhost:3000/bookmarks",this.userData).then(save_response => {
+            this.$appAxios.post("/bookmarks",this.userData).then(save_response => {
                 // getting user data information into the db
                 console.log('save_response', save_response)
                 this.resetData(this.userData);
