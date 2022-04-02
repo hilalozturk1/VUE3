71728334d433bfd7828ef8ce0e9ea17f186de4f1
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-02 20:21:38 +0300

    routing with the received value

--- 15-vue-router/src/views/HomePage.vue
+++ 15-vue-router/src/views/HomePage.vue
@@ -1,3 +1,13 @@
 <template>
     <h3>Home</h3>
+    <input type="text" @keydown.enter="goToDetails">
 </template>
+<script>
+export default {
+    methods: {
+        goToDetails(event){
+            this.$router.push("/details/"+event.target.value);
+        }
+    },
+}
+</script>
