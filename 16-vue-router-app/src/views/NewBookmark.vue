1f59274f60b470b008e11def3e179281090f0bf4
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-02 22:14:55 +0300

    save data

--- 16-vue-router-app/src/views/NewBookmark.vue
+++ 16-vue-router-app/src/views/NewBookmark.vue
@@ -2,17 +2,35 @@
     <div class="form-area card border p-2">
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" placeholder="bookmark..">
+            <input type="text" class="form-control" id="title" placeholder="bookmark.." v-model="userData.title">
         </div>
         <div class="mb-3">
             <label for="url" class="form-label">URL</label>
-            <input type="text" class="form-control" id="url" placeholder="https://...">
+            <input type="text" class="form-control" id="url" placeholder="https://..." v-model="userData.url">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" id="description" rows="3" placeholder="description"></textarea>
+            <textarea class="form-control" id="description" rows="3" placeholder="description" v-model="userData.description"></textarea>
         </div>
         <button class="btn btn-sm btn-secondary" @click="$router.push({name:'HomePage'})">Cancel</button>
-        <button class="btn btn-sm btn-primary">Save</button>
+        <button class="btn btn-sm btn-primary" @click="onSave">Save</button>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            userData:{
+                title: null,
+                url: null,
+                description: null
+            }
+        }
+    },
+    methods: {
+        onSave(){
+            console.log('this.userData', this.userData)
+        }
+    },
+}
+</script>
