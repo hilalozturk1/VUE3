7820c07262a999db799c5e7f13092058640e1707
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-02 23:33:47 +0300

    remove directly access axios

--- 16-vue-router-app/src/main.js
+++ 16-vue-router-app/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
-import axios from "axios";//like plugin
+//import axios from "axios";//like plugin
 import {appAxios} from './utils/appAxios';
 const app = createApp(App);
 app.use(router);
-app.config.globalProperties.$axios = axios;//global
+//app.config.globalProperties.$axios = axios;//global
 app.config.globalProperties.$appAxios = appAxios;//global
 app.mount('#app');
