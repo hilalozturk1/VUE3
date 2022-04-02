26ff33237b7e11e65f1fab272a6f706819e53325
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-02 20:03:44 +0300

    active-class

--- 15-vue-router/src/App.vue
+++ 15-vue-router/src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
-  <router-view></router-view>
+  <div class="container">
+    <router-link to="/" class="nav-link" active-class="active">Home</router-link>
+    <router-link to="/about" class="nav-link" active-class="active">About</router-link>
+    <router-view></router-view>
+  </div>
 </template>
+<style >
+  .nav-link{
+    padding: 5px 10px;
+    text-decoration: none;
+    border: 1px solid #fa6558;
+    color: #fa6558;
+    margin-right: 5px;
+    display: inline-block;
+    margin-top: 5px;
+  }
+  .active {
+    background-color: #fa6558;
+    color: white;
+  }
+</style>
