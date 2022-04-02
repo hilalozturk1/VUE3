dedb791da48fe01aae5f952f05f851d94dfa1c52
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-02 22:00:36 +0300

    bookmark app

---
+++ 16-vue-router-app/src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="btn btn-primary btn-sm" @click="$router.push({name:'NewBookmark'})">+ New Add</button>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">URL</th>
+                <th scope="col">Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="i in 10" :key="i">
+                <th scope="row">1</th>
+                <td>Vue3</td>
+                <td>https://v3.vuejs.org</td>
+                <td>
+                    <button class="btn btn-sm btn-danger">Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
---
+++ 16-vue-router-app/src/views/NewBookmark.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="form-area card border p-2">
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" placeholder="bookmark..">
+        </div>
+        <div class="mb-3">
+            <label for="url" class="form-label">URL</label>
+            <input type="text" class="form-control" id="url" placeholder="https://...">
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea class="form-control" id="description" rows="3" placeholder="description"></textarea>
+        </div>
+        <button class="btn btn-sm btn-secondary" @click="$router.push({name:'HomePage'})">Cancel</button>
+        <button class="btn btn-sm btn-primary">Save</button>
+    </div>
+</template>
