055fb11e8ca1b96d51dc1e9a40c9a87159b60561
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-02 23:32:22 +0300

    delete bookmark into the db with axios

--- 16-vue-router-app/src/views/HomePage.vue
+++ 16-vue-router-app/src/views/HomePage.vue
@@ -1,11 +1,14 @@
 <template>
-    <button class="btn btn-primary btn-sm" @click="$router.push({name:'NewBookmark'})">+ New Add</button>
+    <button class="btn btn-primary btn-sm" @click="$router.push({name:'NewBookmark'})">
+        + New Add
+    </button>
     <table class="table table-hover">
         <thead>
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Title</th>
                 <th scope="col">URL</th>
+                <th scope="col">Description</th>
                 <th scope="col">Delete</th>
             </tr>
         </thead>
@@ -17,7 +20,10 @@
                     <a :href="i.url" target="_blank"> {{i.url}} </a>
                 </td>
                 <td>
-                    <button class="btn btn-sm btn-danger">Delete</button>
+                    {{i.description}}
+                </td>
+                <td>
+                    <button class="btn btn-sm btn-danger" @click="deleteBookmark(i)">Delete</button>
                 </td>
             </tr>
         </tbody>
@@ -35,6 +41,16 @@ export default {
             this.bookmarkList = bookmarks_list_response.data || []
             //console.log('bookmarks_list_response', bookmarks_list_response)
         })
+    },
+    methods: {
+        deleteBookmark(bookmark){
+            this.$appAxios.delete("/bookmarks/"+bookmark.id).then(delete_response => {
+                console.log('delete_response', delete_response)
+                if(delete_response.status === 200){
+                    this.bookmarkList = this.bookmarkList.filter((i) => i.id != bookmark.id);
+                }
+            })
+        }
     },
 }
 </script>
