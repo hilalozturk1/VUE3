43bbb11c2c1359b41217a22ac22e80c8c3c17b8e
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-02 22:17:50 +0300

    using axios globally

--- 16-vue-router-app/src/main.js
+++ 16-vue-router-app/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import axios from "axios";//like plugin
 
 const app = createApp(App);
 app.use(router);
+app.config.globalProperties.$axios = axios;//global
 app.mount('#app');
