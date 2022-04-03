dccecd2f8c67838f063994a1790bc8dfcd30d2d2
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-03 15:30:00 +0300

    add axios instance globally

--- 17-socialmark-app/src/main.js
+++ 17-socialmark-app/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import appAxios from './utils/appAxios'
 
 import "@/assets/style.css"
 import appHeader from "@/components/Common/appHeader"//global registering
@@ -10,4 +11,5 @@ const app = createApp(App);
 app.component("AppHeader", appHeader);
 app.component("appBookmarkList", appBookmarkList);
 app.use(router);
+app.config.globalProperties.$appAxios = appAxios;
 app.mount('#app');
