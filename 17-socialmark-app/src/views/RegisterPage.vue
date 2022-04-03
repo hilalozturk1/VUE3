73791b6ad921d1b79bde08736e57c43f4c510baa
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-03 17:21:13 +0300

    encrypting the user password

--- 17-socialmark-app/src/views/RegisterPage.vue
+++ 17-socialmark-app/src/views/RegisterPage.vue
@@ -1,13 +1,35 @@
 <template>
     <div class="login_register_container">
       <h3 class="text-2xl text-center mb-3">Sing Up</h3>
-      <input type="text" placeholder="Fullname" class="input mb-3" />
-      <input type="text" placeholder="Username" class="input mb-3" />
-      <input type="password" placeholder="Password" class="input mb-3" />
-      <button class="default-button">Sign up</button>
+      <input v-model="userData.fullname" type="text" placeholder="Fullname" class="input mb-3" />
+      <input v-model="userData.username" type="text" placeholder="Username" class="input mb-3" />
+      <input v-model="userData.password" type="password" placeholder="Password" class="input mb-3" />
+      <button class="default-button" @click="onSave">Sign up</button>
       <span class="text-center mt-3 text-sm">
         I'm already a member,
-        <a href="#" class="text-red-900 hover:text-black">Log in!</a>
+        <button @click="$router.push({name :'LoginPage'})" class="text-red-900 hover:text-black">Log in!</button>
       </span>
     </div>
 </template>
+<script>
+import CryptoJS from "crypto-js" 
+export default {
+  data() {
+    return {
+      userData : {
+        username : null,
+        fullname : null,
+        password : null
+      }
+    }
+  },
+  methods: {
+    onSave(){
+      const password = this.userData.password;
+      const key =  "booklikle123?"
+      const cryptedPassword = CryptoJS.AES.encrypt(password, key).toString();
+      console.log(cryptedPassword);
+    }
+  },
+}
+</script>
