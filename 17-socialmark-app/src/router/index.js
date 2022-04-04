2f41745e7f255ac2a2d3d9dade1b870f38821dd5
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-04 21:20:35 +0300

    edit the permissions between the pages

--- 17-socialmark-app/src/router/index.js
+++ 17-socialmark-app/src/router/index.js
@@ -30,7 +30,9 @@ const router = createRouter({
 });
 router.beforeEach((to,from,next) => {
     const authRequiredRoutes = ["HomePage"];
+    const authNotRequiredRoutes = ["LoginPage", "RegisterPage"];
     const _isAuthenticated = store.getters._isAuthenticated;
+    if(authNotRequiredRoutes.indexOf(to.name) > -1 && _isAuthenticated) next(false);
     if(authRequiredRoutes.indexOf(to.name) > -1){
         if(_isAuthenticated) next();
         else next({ name : "LoginPage"})
