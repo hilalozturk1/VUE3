cf8a80d15c935c7523b43803ebae5888c8202472
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-09 13:28:36 +0300

    use of ref

--- 19-composition-api/src/App.vue
+++ 19-composition-api/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <h3> {{title}} </h3>
   <input type="text" name="" id="" v-model="title">
+  <p v-if="show">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Excepturi aliquam nesciunt vero facilis deserunt odit debitis magni animi, 
+    architecto provident quisquam doloremque, aspernatur explicabo, 
+    voluptates laborum labore accusantium vitae dolor?
+  </p>
+  <button @click="show = !show">Toggle</button>
 </template>
 <script>
 import { ref } from "vue";
@@ -14,8 +21,10 @@ export default {
     //setup funk. being used instead of data-beforeCreate-created
     //purpose of setup is to work before without creation data reactivity and to create data
     const title = ref("Setup Title") // needs to return an object
+    const show = ref(false);
     return {
       title,
+      show
     }
   }
 }
