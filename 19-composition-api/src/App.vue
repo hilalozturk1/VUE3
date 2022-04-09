ce33850363a25c0add411c4a49a8d8cfa6532547
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-09 13:24:46 +0300

    composition api feature: ref -> allows defining a reactive variable

--- 19-composition-api/src/App.vue
+++ 19-composition-api/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <h3> {{title}} </h3>
+  <input type="text" name="" id="" v-model="title">
 </template>
 <script>
+import { ref } from "vue";
 export default {
   // data() {
   //   return {
@@ -11,7 +13,7 @@ export default {
   setup() {//to use composition api together with option api 
     //setup funk. being used instead of data-beforeCreate-created
     //purpose of setup is to work before without creation data reactivity and to create data
-    const title = "Setup Title" // needs to return an object
+    const title = ref("Setup Title") // needs to return an object
     return {
       title,
     }
