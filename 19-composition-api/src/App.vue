192fc6c4ef70d2983a4cbd51fd18bcb9eda0083c
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-09 13:18:50 +0300

    setup method

--- 19-composition-api/src/App.vue
+++ 19-composition-api/src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  
+  <h3> {{title}} </h3>
 </template>
+<script>
+export default {
+  // data() {
+  //   return {
+  //     title : "Title"
+  //   }
+  // },
+  setup() {//to use composition api together with option api 
+    //setup funk. being used instead of data-beforeCreate-created
+    //purpose of setup is to work before without creation data reactivity and to create data
+    const title = "Setup Title" // needs to return an object
+    return {
+      title,
+    }
+  }
+}
+</script>
