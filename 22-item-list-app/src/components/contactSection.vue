3e683f27ab35cbc61ceffc1a7fbc6e3d81da3448
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-10 18:28:45 +0300

    make contactSection comp dynamic

--- 22-item-list-app/src/components/contactSection.vue
+++ 22-item-list-app/src/components/contactSection.vue
@@ -2,26 +2,29 @@
   <div>
     <label for="client_name" class="flex flex-col text-gray-400 mb-2">
       <small class="mb-1">Customer Name</small>
-      <input autocomplete="off" id="client_name" class="input" type="text" />
+      <input v-model="contact.contactName" autocomplete="off" id="client_name" class="input" type="text" />
     </label>
     <label for="client_email" class="flex flex-col text-gray-400 mb-2">
       <small class="mb-1">E-mail</small>
-      <input autocomplete="off" id="client_email" class="input" type="email" />
+      <input v-model="contact.email" autocomplete="off" id="client_email" class="input" type="email" />
     </label>
 
     <div class="grid grid-cols-3 gap-5">
       <label for="city" class="flex flex-col text-gray-400 mb-2">
         <small class="mb-1">City</small>
-        <input autocomplete="off" id="city" class="input" type="text" />
+        <input v-model="contact.city" autocomplete="off" id="city" class="input" type="text" />
       </label>
       <label for="zip_code" class="flex flex-col text-gray-400 mb-2">
         <small class="mb-1">Zip Code</small>
-        <input autocomplete="off" id="zip_code" class="input" type="text" />
+        <input v-model="contact.zipcode" autocomplete="off" id="zip_code" class="input" type="text" />
       </label>
       <label for="country" class="flex flex-col text-gray-400 mb-2">
         <small class="mb-1">Country</small>
-        <input autocomplete="off" id="country" class="input" type="text" />
+        <input v-model="contact.country" autocomplete="off" id="country" class="input" type="text" />
       </label>
     </div>
   </div>
 </template>
+<script setup>
+defineProps({ contact: Object });
+</script>
