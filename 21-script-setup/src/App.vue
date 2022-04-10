5b9715ca1f56b85af59502ae0775fa48f1eaa2fd
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-10 15:47:02 +0300

    script setup/example reactive - composition api

--- 21-script-setup/src/App.vue
+++ 21-script-setup/src/App.vue
@@ -4,11 +4,25 @@
   <button @click="inc">{{ counter }}</button>
   <hr />
   <oddOrEven :counter="counter" @odd-event="alertMe" />
+  <hr />
+  <h1>User App</h1>
+  <input type="text" name="" id="" v-model="state.personal.name" />
+  <input type="text" name="" id="" v-model="state.personal.lname" />
+  {{ state.personal }}
 </template>
 <script setup>
 // beforeCreate() created() and data()
-import oddOrEven from "./components/oddOrEven.vue"
-import Utils from "./composables/Utils.js"
-
-const {title, counter, inc, alertMe} = Utils();
+import { ref, reactive } from "vue";
+import oddOrEven from "./components/oddOrEven.vue";
+import Utils from "./composables/Utils.js";
+const { title, counter, inc, alertMe } = Utils();
+//User App
+// const name = ref("");
+// const lname = ref("");
+const state = reactive({
+  personal: {
+    name: null,
+    lname: null,
+  },
+});
 </script>
