3c795a1ee0e9094bd0ce8f97977b0c8da71f562c
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-10 18:22:31 +0300

    prepare the data

--- 22-item-list-app/src/components/invoiceContent.vue
+++ 22-item-list-app/src/components/invoiceContent.vue
@@ -3,11 +3,11 @@
     class="bg-gray-900 w-1/3 mx-auto mt-10 p-2 px-5 rounded-md shadow-2xl"
   >
     <heading title="Invoice Info"/>
-    <contact-section />
+    <contact-section :contact="state.contact"/>
 
     <div class="mt-5">
       <heading title="Invoice Items"/>
-      <invoice-items />
+      <invoice-items :items="state.items"/>
     </div>
     <!-- Summary -->
     <invoice-summary />
@@ -21,4 +21,15 @@
 import invoiceItems from "./invoiceItems.vue"
 import invoiceSummary from "./invoiceSummary.vue"
 import contactSection from "./contactSection.vue"
+import {reactive} from "vue";
+const state = reactive({
+  contact : {
+    contactName : null,
+    email : null,
+    city : null,
+    country : null,
+    zipcode : null
+  },
+  items : []
+})
 </script>
