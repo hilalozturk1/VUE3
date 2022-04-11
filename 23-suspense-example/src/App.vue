70d0aa9369f86f723f30193e8ff233071d459e6e
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-11 15:28:16 +0300

    using - async&onErrorCapture

--- 23-suspense-example/src/App.vue
+++ 23-suspense-example/src/App.vue
@@ -1,25 +1,28 @@
 <template>
-  <div style="display: flex; float: left">
+  <div>
     <span v-if="err" class="error">{{err}}</span>
     <suspense v-else>
       <template #default>
-        <Todos /> <!-- async req -->
+        <div style="display: flex; float: left">        
+            <Todos /> <!-- async req -->
+            <hr />
+            <Users />
+        </div>
       </template>
       <template #fallback>
         <div>Loading..</div>
       </template>
     </suspense>
-    <hr />
-    <Users />
   </div>
 </template>
 
 <script setup>
 import { onErrorCaptured,defineAsyncComponent, ref} from "@vue/runtime-core";
 //import Todos from "./components/Todos.vue";//loading sync
-import Users from "./components/Users.vue";
+//import Users from "./components/Users.vue";
 const err = ref(null);
 const Todos = defineAsyncComponent(() => import("./components/Todos.vue"));// loading comp. async
+const Users = defineAsyncComponent(() => import("./components/Users.vue"));// loading comp. async
 onErrorCaptured((e) => {
     err.value = e;
     return true;
