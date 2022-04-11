e6b05096fe2c6b786a3f82375e9036811d68d9f8
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-11 14:16:13 +0300

    fetch data with jsonplaceholder.typicode

---
+++ 23-suspense-example/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="display:flex;float:left">
+    <Todos />
+    <hr />
+    <Users />
+  </div>
+</template>
+
+<script setup>
+import Todos from "./components/Todos.vue";
+import Users from "./components/Users.vue";
+</script>
