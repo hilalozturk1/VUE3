a504151866ae1877f7d2e5e00bb52da4874230ce
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-11 22:52:31 +0300

    convert login page & store, router

--- 26-socialmark-composition-api/src/views/LoginPage.vue
+++ 26-socialmark-composition-api/src/views/LoginPage.vue
@@ -12,34 +12,32 @@
     </span>
   </div>
 </template>
-<script>
+<script setup>
+import { reactive, inject } from "vue";
+import { useStore} from "vuex";
+import { useRouter } from "vue-router";
 import CryptoJS from "crypto-js" 
-export default {
-  data() {
-    return {
-      userData : {
-        username : null,
-        password : null
+const appAxios = inject("appAxios");
+const store = useStore();
+const router = useRouter();
+const userData = reactive({
+  username : null,
+  password : null
+});
+const onSubmit = () => {
+  // GET /post?title=json-server&author=typicode
+  const password = CryptoJS.HmacSHA1(userData.password, store.getters._saltKey).toString();
+  appAxios.get("/users?username="+userData.username+"&password="+password)
+    .then(login_response => {
+      if(login_response?.data?.length > 0){
+        store.commit("setUser", login_response?.data[0])//update user the app level
+        router.push({name: "HomePage"})
       }
-    }
-  },
-  methods: {
-    onSubmit(){
-      // GET /post?title=json-server&author=typicode
-      const password = CryptoJS.HmacSHA1(this.userData.password, this.$store.getters._saltKey).toString();
-      this.$appAxios.get("/users?username="+this.userData.username+"&password="+password)
-        .then(login_response => {
-          if(login_response?.data?.length > 0){
-            this.$store.commit("setUser", login_response?.data[0])//update user the app level
-            this.$router.push({name: "HomePage"})
-          }
-          else{
-            alert("The user was not found..")
-          }
-          console.log('login_response', login_response)
-        })
-        .catch(e => console.log('e', e));
-    }
-  },
+      else{
+        alert("The user was not found..")
+      }
+      console.log('login_response', login_response)
+    })
+    .catch(e => console.log('e', e));
 }
 </script>
