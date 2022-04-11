99dce1aa4b1d34905063e184a2e6cacc1ac95fe5
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-11 15:23:23 +0300

    onErrorCaptured func / only vue3 / async comp catch err

--- 23-suspense-example/src/App.vue
+++ 23-suspense-example/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="display: flex; float: left">
-    <suspense>
+    <span v-if="err" class="error">{{err}}</span>
+    <suspense v-else>
       <template #default>
         <Todos /> <!-- async req -->
       </template>
@@ -14,8 +15,18 @@
 </template>
 
 <script setup>
-import {defineAsyncComponent} from "@vue/runtime-core";
+import { onErrorCaptured,defineAsyncComponent, ref} from "@vue/runtime-core";
 //import Todos from "./components/Todos.vue";//loading sync
 import Users from "./components/Users.vue";
+const err = ref(null);
 const Todos = defineAsyncComponent(() => import("./components/Todos.vue"));// loading comp. async
+onErrorCaptured((e) => {
+    err.value = e;
+    return true;
+})
 </script>
+<style>
+.error {
+    color: red;
+}
+</style>
