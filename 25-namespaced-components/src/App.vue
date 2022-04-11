58a314932e4f95d2154415a8eab270165c1b7a5e
hilalozturk1 <hlloztrk68@gmail.com>
2022-04-11 16:25:51 +0300

    usage namespaced components

---
+++ 25-namespaced-components/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+// import Input from "./components/Forms/Input.vue"
+// import Label from "./components/Forms/Label.vue"
+// import Select from "./components/Forms/Select.vue"
+import * as Form from "./components/form-components";
+</script>
+<template>
+  <!-- <Label>
+    Title
+    <Input />
+  </Label>
+  <Label>
+    Category
+    <Select />
+  </Label> -->
+  <Form.Label>
+    Title
+    <Form.Input />
+  </Form.Label>
+  <Form.Label>
+    Category
+    <Form.Select />
+  </Form.Label>
+</template>
---
+++ 25-namespaced-components/src/components/Forms/Select.vue
@@ -0,0 +1,8 @@
+<template>
+  <slot />
+  <select>
+      <option value="1">Opt 1</option>
+      <option value="2">Opt 2</option>
+      <option value="3">Opt 3</option>
+  </select>
+</template>
